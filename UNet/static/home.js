66860d09d1acad9325c4e66aa1af3788d0377d02
Chrunch-tech <uday014@gmail.com>
2021-01-18 22:36:48 +0530

    Update html and js file

--- UNet/static/home.js
+++ UNet/static/home.js
@@ -1,36 +1,35 @@
 const brand_name = document.getElementById("brand-name");
-const login_link = document.getElementById("login-link")
-const register_link = document.getElementById("register-link")
+const login_link = document.getElementById("login-link");
+const register_link = document.getElementById("register-link");
 
-
-brand_name.addEventListener("mouseover", ()=>{
-    brand_name.id = "brand-name-hover-active";
+brand_name.addEventListener("mouseover", () => {
+  brand_name.id = "brand-name-hover-active";
 });
 
-brand_name.addEventListener("mouseout", ()=>{
-    brand_name.id = "brand-name";
+brand_name.addEventListener("mouseout", () => {
+  brand_name.id = "brand-name";
 });
 
-login_link.addEventListener("mouseover", ()=>{
-    login_link.id = "login-link-hover";
+login_link.addEventListener("mouseover", () => {
+  login_link.id = "login-link-hover";
 });
 
-login_link.addEventListener("mouseout", ()=>{
-    login_link.id = "login-link";
+login_link.addEventListener("mouseout", () => {
+  login_link.id = "login-link";
 });
 
-register_link.addEventListener("mouseover", ()=>{
-    register_link.id = "register-link-hover";
+register_link.addEventListener("mouseover", () => {
+  register_link.id = "register-link-hover";
 });
 
-register_link.addEventListener("mouseout", ()=>{
-    register_link.id = "register-link";
+register_link.addEventListener("mouseout", () => {
+  register_link.id = "register-link";
 });
 
-login_link.addEventListener("click", ()=>{
-    login_link.id = "login-link-click";
+login_link.addEventListener("click", () => {
+  login_link.id = "login-link-click";
 });
 
-register_link.addEventListener("click", ()=>{
-    register_link.id = "register-link-click";
-})
+register_link.addEventListener("click", () => {
+  register_link.id = "register-link-click";
+});
